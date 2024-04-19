d4faa84814583d3d4403371943719e76f5de22a5
Anja Petry <anjapetry@protonmail.com>
2024-04-19 21:14:25 +0200

    chore:add gsap

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,6 @@
-import { Content } from "@prismicio/client";
+import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-
+import gsap from "gsap";
 /**
  * Props for `Hero`.
  */
@@ -10,15 +10,45 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+    const renderLetters = (name: KeyTextField, key: string) => {
+        if (!name) return;
+        return name.split("").map((letter, index) => (
+            <span
+                key={index}
+                className={`name-animation name-animation-${key} inline-block opacity-0`}
+            >
+                {letter}
+            </span>
+        ));
+    };
+
     return (
         <section
             data-slice-type={slice.slice_type}
             data-slice-variation={slice.variation}
         >
             {/* Placeholder component for hero (variation: {slice.variation}) Slices */}
-            <>{slice.primary.first_name}</>
-            <>{slice.primary.last_name}</>
-            <>{slice.primary.tag_line}</>
+
+            <div className='grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center'>
+                <div className='col-start-1 md:row-start-1'>
+                    <h1
+                        className='mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-[1.05em] tracking-tighter'
+                        aria-label={`$
+                        {slice.primary.first_name}
+                        {slice.primary.last_name}`}
+                    >
+                        <span className='block text-orange-100 ml-6'>
+                            {renderLetters(slice.primary.first_name, "first")}
+                        </span>
+                        <span className='-mt-[.2em] block text-emerald-300'>
+                            {renderLetters(slice.primary.last_name, "last")}
+                        </span>
+                    </h1>
+                    <span className='block bg-gradient-to-tr from-yellow-500 via-yellow-300 to-amber-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity.100 md:text-4xl'>
+                        {slice.primary.tag_line}
+                    </span>
+                </div>
+            </div>
         </section>
     );
 };
