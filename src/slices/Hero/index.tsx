2732c470e53903b8d9e11636a0283ecaf9bd306f
Anja Petry <anjapetry@protonmail.com>
2024-04-22 18:19:09 +0200

    set up Three.js scene with a range of different clickable geometrical shapes in Shapes.tsx

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -86,19 +86,19 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
                 <Shapes />
                 <div className='col-start-1 md:row-start-1'>
                     <h1
-                        className='mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-[1.05em] tracking-tighter'
+                        className='mx-auto mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-[1.05em] tracking-tighter'
                         aria-label={`$
                         {slice.primary.first_name}
                         {slice.primary.last_name}`}
                     >
-                        <span className='block text-orange-100 ml-6'>
+                        <span className='block text-orange-100 ml-10'>
                             {renderLetters(slice.primary.first_name, "first")}
                         </span>
                         <span className='-mt-[.2em] block text-[#00bae2]'>
                             {renderLetters(slice.primary.last_name, "last")}
                         </span>
                     </h1>
-                    <span className='job-title block bg-gradient-to-tr from-yellow-500 via-yellow-300 to-amber-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl'>
+                    <span className='job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-amber-600 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl'>
                         {slice.primary.tag_line}
                     </span>
                 </div>
