60cdae7c8fa6047d2935c0663b49663e44ece001
Anja Petry <anjapetry@protonmail.com>
2024-04-23 11:39:30 +0200

    feat:create Heading.tsx in components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang='en' className='bg-slate-900 text-slate-100'>
+        <html lang='en' className='bg-slate-900'>
             <body className={clsx(urbanist.className, "relative min-h-screen")}>
                 <Header />
                 {children}
--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -1,3 +1,4 @@
+import Bounded from "@/components/Bounded";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
@@ -10,14 +11,14 @@ export type BiographyProps = SliceComponentProps<Content.BiographySlice>;
  * Component for "Biography" Slices.
  */
 const Biography = ({ slice }: BiographyProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for biography (variation: {slice.variation}) Slices
-    </section>
-  );
+    return (
+        <Bounded
+            data-slice-type={slice.slice_type}
+            data-slice-variation={slice.variation}
+        >
+            <div className='grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]'></div>
+        </Bounded>
+    );
 };
 
 export default Biography;
