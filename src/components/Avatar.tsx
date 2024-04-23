3c92a945bb539e633a071973dc59cdb40c948e6b
Anja Petry <anjapetry@protonmail.com>
2024-04-23 13:39:19 +0200

    style:update Avatar and Button comps

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,84 @@
 "use client";
 
-import { PrismicNextImage } from "@prismicio/react";
-import { ImageField } from "@prismicio/types";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ImageField } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
 import clsx from "clsx";
+import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
 
-type AvatarProps = {
+export default function Avatar({
+    image,
+    className,
+}: {
     image: ImageField;
     className?: string;
-};
-
-export default function Avatar({ image, className }: AvatarProps) {
+}) {
     const component = useRef(null);
+    const prefersReducedMotion = usePrefersReducedMotion();
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.fromTo(
+                ".avatar",
+                {
+                    opacity: 0,
+                    scale: 1.4,
+                },
+                {
+                    scale: 1,
+                    opacity: 1,
+                    duration: prefersReducedMotion ? 0 : 1.3,
+                    ease: "power3.inOut",
+                }
+            );
+
+            window.onmousemove = (e) => {
+                if (!component.current) return; // no component, no animation!
+                const componentRect = (
+                    component.current as HTMLElement
+                ).getBoundingClientRect();
+                const componentCenterX =
+                    componentRect.left + componentRect.width / 2;
+
+                let componentPercent = {
+                    x: (e.clientX - componentCenterX) / componentRect.width / 2,
+                };
+
+                let distFromCenterX = 1 - Math.abs(componentPercent.x);
+
+                gsap.timeline({
+                    defaults: {
+                        duration: 0.5,
+                        overwrite: "auto",
+                        ease: "power3.out",
+                    },
+                })
+                    .to(
+                        ".avatar",
+                        {
+                            rotation: gsap.utils.clamp(
+                                -2,
+                                2,
+                                5 * componentPercent.x
+                            ),
+                            duration: 0.5,
+                        },
+                        0
+                    )
+                    .to(
+                        ".highlight",
+                        {
+                            opacity: distFromCenterX - 0.7,
+                            x: -10 + 20 * componentPercent.x,
+                            duration: 0.5,
+                        },
+                        0
+                    );
+            };
+        }, component);
+        return () => ctx.revert(); // cleanup!
+    }, [prefersReducedMotion]);
 
     return (
         <div
@@ -19,7 +86,7 @@ export default function Avatar({ image, className }: AvatarProps) {
             className={clsx("relative h-full w-full", className)}
         >
             <div
-                className='avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0'
+                className='avatar aspect-w-1 aspect-h-1 overflow-hidden rounded-3xl border-2 border-stone-500 opacity-0'
                 style={{ perspective: "500px", perspectiveOrigin: "150% 150%" }}
             >
                 <PrismicNextImage
@@ -27,6 +94,7 @@ export default function Avatar({ image, className }: AvatarProps) {
                     className='avatar-image h-full w-full object-fill'
                     imgixParams={{ q: 90 }}
                 />
+                <div className='highlight absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via-amber-400 to-transparent opacity-0 md:block'></div>
             </div>
         </div>
     );
--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -1,13 +1,10 @@
-import Bounded from "@/components/Bounded";
-import Heading from "@/components/Heading";
-import Button from "@/components/Button";
 import Avatar from "@/components/Avatar";
+import Bounded from "@/components/Bounded";
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
-import {
-    PrismicRichText,
-    PrismicNextImage,
-    SliceComponentProps,
-} from "@prismicio/react";
+
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Biography`.
@@ -27,17 +24,20 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
                 <Heading as='h1' size='xl' className='col-start-1'>
                     {slice.primary.heading}
                 </Heading>
+
                 <div className='prose prose-xl prose-slate prose-invert col-start-1'>
                     <PrismicRichText field={slice.primary.description} />
                 </div>
+
                 <Button
                     linkField={slice.primary.button_link}
                     label={slice.primary.button_text}
                 />
-                  <Avatar
-          image={slice.primary.avatar}
-          className="row-start-1 max-w-sm md:col-start-2 md:row-end-3"
-        />
+
+                <Avatar
+                    image={slice.primary.avatar}
+                    className='row-start-1 max-w-sm md:col-start-2 md:row-end-3'
+                />
             </div>
         </Bounded>
     );
