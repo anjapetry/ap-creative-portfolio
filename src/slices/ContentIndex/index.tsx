6687cdc159a0c049d2bfb21f3df88dae270d3b47
Anja Petry <anjapetry@protonmail.com>
2024-04-24 16:52:03 +0200

    feat:customise ContentIndex/index.tsx

--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -1,5 +1,8 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Content, isFilled } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
 
 /**
  * Props for `ContentIndex`.
@@ -10,15 +13,21 @@ export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
  * Component for "ContentIndex" Slices.
  */
 const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for content_index (variation: {slice.variation})
-      Slices
-    </section>
-  );
+    return (
+        <Bounded
+            data-slice-type={slice.slice_type}
+            data-slice-variation={slice.variation}
+        >
+            <Heading size='xl' className='mb-8'>
+                {slice.primary.heading}
+            </Heading>
+            {isFilled.richText(slice.primary.description) && (
+                <div className='prose prose-xl prose-invert mb-10'>
+                    <PrismicRichText field={slice.primary.description} />
+                </div>
+            )}
+        </Bounded>
+    );
 };
 
 export default ContentIndex;
