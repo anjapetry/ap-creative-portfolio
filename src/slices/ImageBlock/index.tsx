7cc79d8cda52b3130f72459af5318e03450a4dfb
Anja Petry <anjapetry@protonmail.com>
2024-04-25 19:04:00 +0200

    style:modify ImageBlock stylesto match blog posts

--- src/slices/ImageBlock/index.tsx
+++ src/slices/ImageBlock/index.tsx
@@ -1,5 +1,6 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage } from "@prismicio/next";
 
 /**
  * Props for `ImageBlock`.
@@ -10,15 +11,12 @@ export type ImageBlockProps = SliceComponentProps<Content.ImageBlockSlice>;
  * Component for "ImageBlock" Slices.
  */
 const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for image_block (variation: {slice.variation})
-      Slices
-    </section>
-  );
+    return (
+        <PrismicNextImage
+            field={slice.primary.image}
+            className='not-prose w-full h-full rounded-md my-10 md:my-14 lg:my-16 border border-stone-600'
+        />
+    );
 };
 
 export default ImageBlock;
